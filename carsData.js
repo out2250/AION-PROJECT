1244db847956f2ee820fcc4f3d8fb7e4dcaa5d75
out2250 <out2250@fifgroup.co.id>
2025-03-22 00:41:18 +0700

    TEST

--- carsData.js
+++ carsData.js
@@ -1,16 +1,16 @@
 const carsData = {
     "AION_Y_PLUS": {
-        "image_path": "./Images/AION_Y_PLUS/AION_Y_PLUS_2.jpeg",
+        "image_path": "./images/AION_Y_PLUS/AION_Y_PLUS_2.jpeg",
         "description": "Mobil listrik canggih dengan desain modern dan teknologi terkini.",
         "colors": ["Sunset Orange", "Galaxy Blue", "Wilderness Sand", "Sea Fluorescent Gray", "Arctic White", "Holographic Silver", "Night Shadow Black", "Sunset Orange White Roof", "Galaxy Blue White Roof"]
     },
     "AION_V": {
-        "image_path": "./Images/AION_V/AION_V_2.jpeg",
+        "image_path": "./images/AION_V/AION_V_2.jpeg",
         "description": "Mobil listrik dengan performa tinggi dan efisiensi energi.",
         "colors": ["Beige", "Black", "Brown", "Orange"]
     },
     "HYPTEC_HT": {
-        "image_path": "./Images/HYPTEC_HT/HYPTECT_HT_1.jpeg", // Corrected file path
+        "image_path": "./images/HYPTEC_HT/HYPTECT_HT_1.jpeg", // Corrected file path
         "description": "Mobil listrik dengan teknologi canggih dan kenyamanan maksimal.",
         "colors": ["Sunset Orange", "Galaxy Blue", "Wilderness Sand", "Sea Fluorescent Gray", "Arctic White", "Holographic Silver", "Night Shadow Black", "Sunset Orange White Roof", "Galaxy Blue White Roof"]
     }
